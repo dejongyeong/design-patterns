d123a17b7618444122cfe601e647499bec1219a6
De Jong <dejong.re2@gmail.com>
2018-09-23 01:37:58 +0100

    Skeleton Code for Customer Test Class
    Adds skeleton code for testing customer class, imports required package from EasyMock

---
+++ test/java/CustomersTest.java
@@ -0,0 +1,8 @@
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.verify;
+
+public class CustomersTest {
+
+}
